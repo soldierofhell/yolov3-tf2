128ae1403e1aad0c2088062e6b29969cc52a3cec
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-11-13 14:48:53 +0100

    Update dataset.py

--- yolov3_tf2/dataset.py
+++ yolov3_tf2/dataset.py
@@ -111,7 +111,7 @@ def parse_tfrecord(tfrecord, class_table):
                         tf.sparse.to_dense(x['image/object/bbox/ymax']),
                         labels], axis=1)
     
-    paddings = [[0, 500 - tf.shape(y_train)[0]], [0, 0]]
+    paddings = [[0, 1000 - tf.shape(y_train)[0]], [0, 0]]
     y_train = tf.pad(y_train, paddings)
 
     return x_train, y_train
