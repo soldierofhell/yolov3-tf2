c6a5ea4fa1864fad62f7d32345901bab1507d63d
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-11-13 10:24:55 +0100

    Update convert.py

--- convert.py
+++ convert.py
@@ -25,7 +25,7 @@ def main(_argv):
     output = yolo(img)
     logging.info('sanity check passed')
 
-    yolo.save_weights(FLAGS.output)
+    yolo.save_weights(FLAGS.output, save_format='h5')
     logging.info('weights saved')
 
 
